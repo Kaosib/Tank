ea3e99cc4017b437d5877be46df06d2fa423bec4
Kaosib <42632280+Kaosib@users.noreply.github.com>
2018-08-30 15:06:37 +0700

    Tank

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1535616205,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/particles.png",
+		"images/bullet-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/wall2-sheet0.png",
+		"images/jeep-sheet0.png",
+		"images/holp-sheet0.png",
+		"images/ef-sheet0.png",
+		"images/eff.png",
+		"images/home-sheet0.png",
+		"images/holphome-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
